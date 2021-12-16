70235bdb126c2a7cd846545a23adfcd5b2785ad3
unknown <steve.potts@bowlereggs.co.uk>
2021-12-16 22:02:13 +0000

    start of s3

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
+import Header from "./components/Header";
+
 function App() {
-  const title = "Blog Post";
-  const body = "Blog post data!";
-  const loading = false;
-  const showComments = true;
-
-  const comments = [
-    { id: 1, text: "Comment 1" },
-    { id: 2, text: "Comment 2" },
-    { id: 3, text: "Comment 3" },
-  ];
-
-  if (loading) return <h1>Loading...</h1>;
-
   return (
     <>
-      <h1>{title}</h1>
-      <h4>{body}</h4>
-
-      {showComments ? (
-        <div className="comments">
-          <h3>Comments ({comments.length})</h3>
-          <ul>
-            {comments.map((comment, index) => (
-              <li key={index}>{comment.text}</li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        "NO"
-      )}
+      <Header />
+      <div className="container">
+        <h1>My app!</h1>
+      </div>
     </>
   );
 }
