5b85ec39cb5f7b0c91e5572852b36a78f94c22d2
unknown <steve.potts@bowlereggs.co.uk>
2021-12-18 14:38:45 +0000

    before context api

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -16,7 +16,7 @@ function FeedbackList({ feedback, handleDelete }) {
           initial={{opacity: 0}}
           animate={{opacity: 1}}
           exit={{opacity: 0}}
-          transition={{duration: 1}}
+          transition={{duration: 0.6}}
           >
             <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
           </motion.div>
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+import Card from "../components/shared/Card"
+
+function AboutPage() {
+  return (
+    <div>
+      <Card>
+        <div className="about">
+          <h1>About this Project</h1>
+          <p>THis is a react app to leave feedback for a product or service.</p>
+
+          <p>Version: 1.0</p>
+
+          <p>
+            <Link to="/">Back to Home</Link>
+          </p>
+        </div>
+      </Card>
+    </div>
+  )
+}
+
+export default AboutPage
