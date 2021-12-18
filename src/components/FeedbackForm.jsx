e0b289f95c94f897f355ea090870e5a592493a2d
unknown <steve.potts@bowlereggs.co.uk>
2021-12-18 00:14:35 +0000

    after validation and fix

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,9 +6,21 @@ import Card from "./shared/Card";
 function FeedbackForm() {
 
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
-  const handleTextChange = (e) => {
-    setText(e.target.value);
+  const handleTextChange = ({target: {value}}) => {
+    if (value === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (value !== '' && value.trim().length < 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be at least 10 Chars');
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(value);
   };
 
   return (
@@ -17,8 +29,9 @@ function FeedbackForm() {
         <h2>How do you rate your experience with us?</h2>
         <div className="input-group">
           <input onChange={handleTextChange} type="text" placeholder="Write a review" value={text}/>
-          <Button type="submit" version="secondary">Send</Button>
+          <Button type="submit" version="secondary" isDisabled={btnDisabled}>Send</Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   )
