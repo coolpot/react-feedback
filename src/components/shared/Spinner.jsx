f4389360826ae87b877758e56b4b5bd7c74ca98e
unknown <steve.potts@bowlereggs.co.uk>
2021-12-18 23:54:35 +0000

    netlifyfix 2

--- src/components/shared/Spinner.jsx
+++ src/components/shared/Spinner.jsx
@@ -2,7 +2,7 @@ import spinner from '../assets/spinner.gif';
 
 
 function Spinner() {
-  return <img src={spinner} style={{width: '100px', margin: 'auto', display: 'block'}} />
+  return <img src={spinner} style={{width: '100px', margin: 'auto', display: 'block'}} alt='Spinner' />
 }
 
 export default Spinner
